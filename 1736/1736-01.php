76c499eb290af88714bd75535a8f08c04fbed2db
Cass Hsu <cass.hsu@gmail.com>
2021-01-28 23:27:36 +0800

    init 1736 & Approach 1

---
+++ 1736/1736-01.php
@@ -0,0 +1,11 @@
+<?php
+class Solution {
+    function maximumTime($time) {
+        if ($time[0] == '?') $time[0] = (!($time[1] == '?') && intval($time[1]) > 3) ? '1' : '2';
+        if ($time[1] == '?') $time[1] = (intval($time[0]) > 1) ? '3' : '9';
+        if ($time[3] == '?') $time[3] = '5';
+        if ($time[4] == '?') $time[4] = '9';
+        
+        return $time;
+    }
+}
