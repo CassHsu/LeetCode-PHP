1b312087f44731d447c77489cdd2c72c8f1f6156
CassHsu <cass.hsu@gmail.com>
2021-04-29 10:39:09 +0800

    Approach 3: Sorting

---
+++ 0169/0169-03.php
@@ -0,0 +1,11 @@
+<?php
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function majorityElement($nums) {
+        sort($nums);
+        return $nums[floor(count($nums) / 2)];
+    }
+}
