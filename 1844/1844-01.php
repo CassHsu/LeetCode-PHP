988fdb9bf308be7dcb5a8393e460a70254e7c892
CassHsu <cass.hsu@gmail.com>
2021-05-12 18:33:20 +0800

    Create 1844-01.php

---
+++ 1844/1844-01.php
@@ -0,0 +1,13 @@
+<?php
+class Solution {
+    /**
+     * @param String $s
+     * @return String
+     */
+    function replaceDigits($s) {
+        for ($i = 1; $i < strlen($s); $i+=2) {
+            $s[$i] = chr(ord($s[$i-1]) + intval($s[$i]));
+        }
+        return $s;   
+    }
+}
