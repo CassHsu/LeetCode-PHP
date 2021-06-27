08f6b3237a51a5a57dbfa2db77b805f6a1198230
CassHsu <cass.hsu@gmail.com>
2021-06-27 14:47:38 +0800

    Approach 1: DP  + Loop

---
+++ 0062/0062-01.php
@@ -0,0 +1,34 @@
+<?php
+class Solution {
+    /**
+     * @param Integer $m
+     * @param Integer $n
+     * @return Integer
+     */
+    function uniquePaths($m, $n) {
+        $dp = array();
+        for ($i = 0; $i < $m; $i++) {
+            $row = array();
+            for ($j = 0; $j < $n; $j++) {
+                array_push($row, 0);
+            }
+            array_push($dp, $row);
+        }
+        
+        for ($i = 0; $i < $m; $i++) {
+            $dp[$i][0] = 1;
+        }
+        
+        for ($i = 0; $i < $n; $i++) {
+            $dp[0][$i] = 1;
+        }
+        
+        for ($i = 1; $i < $m; $i++) {
+            for ($j = 1; $j < $n; $j++) {
+                $dp[$i][$j] = $dp[$i-1][$j] + $dp[$i][$j-1];
+            }
+        }
+        
+        return $dp[$m-1][$n-1];
+    }
+}
