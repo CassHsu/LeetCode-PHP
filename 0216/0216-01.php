f414ec468c4eb753be12770ecc7bc2321f49d84f
CassHsu <cass.hsu@gmail.com>
2021-08-16 14:16:58 +0800

    Approach 1: Backtracking

--- 0216/0216-01.php
+++ 0216/0216-01.php
@@ -1,1 +1,27 @@
-
+<?php
+class Solution {
+    var $ans, $k, $n;
+    function combinationSum3($k, $n) {
+        $this->ans = array();
+        $this->k = $k;
+        $this->n = $n;
+        
+        $this->backtrack(array(), 1, $n);
+        return $this->ans;
+    }
+    
+    function backtrack($curr, $start, $remain) {
+        if (count($curr) === $this->k) {
+            if ($remain === 0) {
+                array_push($this->ans, $curr);
+            }
+            return;
+        }
+        
+        for ($i = $start; $i <= 9; $i++) {
+            array_push($curr, $i);
+            $this->backtrack($curr, $i + 1, $remain - $i);
+            array_pop($curr);
+        }
+    }
+}
