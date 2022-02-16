66f3defd519e105d74529b7446e85923b18f0bb0
CassHsu <cass.hsu@gmail.com>
2022-02-16 09:24:47 +0800

    Create 2169-01.php

---
+++ 2169/2169-01.php
@@ -0,0 +1,18 @@
+<?php
+class Solution {
+    function countOperations($num1, $num2) {
+        $count = 0;
+        while ($num1 > 0 && $num2 > 0) {
+            
+            if ($num1 >= $num2) {
+                $num1 -= $num2;
+            } else {
+                $num2 -= $num1;
+            }
+            
+            $count++;
+        }
+        
+        return $count;
+    }
+}
