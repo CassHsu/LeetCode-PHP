22dbb7df37e40ee14683f1892ccb2914e1c573eb
CassHsu <cass.hsu@gmail.com>
2022-03-03 09:22:39 +0800

    Two Pointers

---
+++ 0392/0392-01.php
@@ -0,0 +1,15 @@
+<?php
+class Solution {
+    function isSubsequence($s, $t) {
+        $i = 0;
+        $j = 0;
+        
+        while ($i < strlen($s) && $j < strlen($t)) {
+            if ($s[$i] === $t[$j]) $i++;
+            
+            $j++;
+        }
+        
+        return $i === strlen($s);
+    }
+}
