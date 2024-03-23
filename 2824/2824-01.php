2a8f73facdc1c4fcb0cc35b39a10bc02cb93f69b
CassHsu <cass.hsu@gmail.com>
2024-03-23 21:17:18 +0800

    Create 2824-01.php

---
+++ 2824/2824-01.php
@@ -0,0 +1,16 @@
+<?
+class Solution {
+    function countPairs($nums, $target) {
+        $count = 0;
+
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = $i + 1; $j < count($nums); $j++) {
+                if (($nums[$i] + $nums[$j]) < $target) {
+                    $count++;
+                }
+            }
+        }
+
+        return $count;
+    }
+}
